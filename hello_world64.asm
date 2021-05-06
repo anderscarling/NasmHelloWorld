a33abb4c1f3ceb52a5c87a57af43553dd1755f76
Anders Carling <anders.carling@makropod.se>
2021-05-06 02:17:52 +0200

    Cleanup a bit

--- hello_world64.asm
+++ hello_world64.asm
@@ -1,21 +1,19 @@
-    SECTION .data
+global start
+
+SECTION .data
+
 msg:    db    "Hello World!", 0x0a
 len:    equ   $-msg
 
-    SECTION .text
-    global start
-
-kernel:
-    syscall
-    ret
+SECTION .text
 
 start:
     mov rax, 0x2000004   ; SYS_write
     mov rdi, 1           ; stdout
     lea rsi, [rel msg]
     mov rdx, len
-    call kernel
+    syscall
 
     mov rax, 0x2000001   ; SYS_exit
     mov rdi, 0
-    call kernel
+    syscall
